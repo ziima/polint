466810631345cf996046fb6f3e419b4014fc628c
Vlastimil Zíma <vlastimil.zima@nic.cz>
2018-11-21 13:41:54 +0100

    Add tox and fix project setup

--- Makefile
+++ Makefile
@@ -1,22 +1,10 @@
 .PHONY: test coverage isort check-isort check-flake8
 
 test:
-	python tests/__init__.py discover
+	tox
 
 coverage:
 	python-coverage erase
 	-rm -r htmlcov
-	python-coverage run --branch --source="." tests/__init__.py discover
+	tox
 	python-coverage html -d htmlcov
-
-isort:
-	isort --recursive polint.py tests
-
-check-isort:
-	isort --check-only --diff --recursive polint.py tests
-
-check-flake8:
-	flake8 --format=pylint polint.py tests
-
-check-doc:
-	pydocstyle polint.py
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+EXTRAS_REQUIRE = {'quality': ['flake8', 'isort', 'pydocstyle'],
+                  'tests': ['mock']}
+
 setup(name='polint',
       version='0.2',
       description='Linter for gettext PO files',
@@ -9,4 +12,5 @@ setup(name='polint',
       py_modules=['polint'],
       entry_points={'console_scripts': ['polint = polint:main']},
       install_requires=['polib'],
+      extras_require=EXTRAS_REQUIRE,
       test_suite='tests')
